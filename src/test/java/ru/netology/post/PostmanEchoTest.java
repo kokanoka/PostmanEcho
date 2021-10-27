ba51b71eee4366f3d781cf00abac0d8ec0e86388
kokanoka <kokanoka30@gmail.com>
2021-10-27 19:32:49 +0300

    fixed test

--- src/test/java/ru/netology/post/PostmanEchoTest.java
+++ src/test/java/ru/netology/post/PostmanEchoTest.java
@@ -17,7 +17,7 @@ public class PostmanEchoTest {
 // Проверки
                 .then()
                 .statusCode(200)
-                .body("data", equalTo("some"))
+                .body("data", equalTo("some data"))
         ;
     }
 }
