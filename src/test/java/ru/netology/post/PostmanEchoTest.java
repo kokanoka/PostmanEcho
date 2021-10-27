ae3063af2d130a63670e1406cd78e6958d99a546
kokanoka <kokanoka30@gmail.com>
2021-10-27 19:29:30 +0300

    failed test

--- src/test/java/ru/netology/post/PostmanEchoTest.java
+++ src/test/java/ru/netology/post/PostmanEchoTest.java
@@ -3,6 +3,7 @@ package ru.netology.post;
 import org.junit.jupiter.api.Test;
 
 import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
 
 public class PostmanEchoTest {
     @Test
@@ -16,7 +17,7 @@ public class PostmanEchoTest {
 // Проверки
                 .then()
                 .statusCode(200)
-                .body()
+                .body("data", equalTo("some"))
         ;
     }
 }
