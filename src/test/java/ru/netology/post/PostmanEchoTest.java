4bd5f8a47ca55c3a7403882338bfdbc0e52bceaf
kokanoka <kokanoka30@gmail.com>
2021-10-27 19:21:52 +0300

    add .gitignore

---
+++ src/test/java/ru/netology/post/PostmanEchoTest.java
@@ -0,0 +1,22 @@
+package ru.netology.post;
+
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+
+public class PostmanEchoTest {
+    @Test
+    void shouldReturnPost () {
+        given()
+                .baseUri("https://postman-echo.com")
+                .body("some data") // отправляемые данные (заголовки и query можно выставлять аналогично)
+// Выполняемые действия
+                .when()
+                .post("/post")
+// Проверки
+                .then()
+                .statusCode(200)
+                .body()
+        ;
+    }
+}
